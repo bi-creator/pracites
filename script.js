2954254795d50b1e9279aa0db32863bb86045ab0
bi-creator <manjithreddy096@gmail.com>
2022-07-27 09:37:11 +0530

    new pages and fetching data from text file in server

--- script.js
+++ script.js
@@ -48,3 +48,36 @@ function homepage(){
   window.location.href="page1.html";
   return false;
 }
+function num() {
+  const inpObj = document.getElementById("id1");
+  if (!inpObj.checkValidity()) {
+    document.getElementById("demo").innerHTML = inpObj.validationMessage;
+  } else {
+    document.getElementById("demo").innerHTML = "Input OK";
+  } 
+} 
+
+
+function fordata() {
+    var x = document.getElementById("abc");
+    var text1 = "";
+    var i;
+    for (i = 0; i < x.length ;i++) {
+      text1 += x.elements[i].value + "<br>";
+    }
+    document.getElementById("em").innerHTML = text1;
+    console.log(text1);
+    let result=text1.replace('<br>', '');;
+    fetch (result)
+    .then(x => x.text())
+    .then(y => document.getElementById("mo").innerHTML = y);
+  }
+
+
+function et(file_name){
+  
+  fetch (file_name)
+  .then(x => x.text())
+  .then(y => document.getElementById("mo").innerHTML = y);
+
+}
